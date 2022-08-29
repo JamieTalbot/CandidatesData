35406af64be4d906c9dbdc467c6220c0e0ede4f5
JamieTalbot <jamietalbot@exe-coll.ac.uk>
2022-08-29 17:37:35 +0100

    Add project files.

---
+++ CandidatesData/CandidatesTable.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE [dbo].[Candidates]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [First_Name] NVARCHAR(100) NOT NULL, 
+    [Last_Name] NVARCHAR(100) NOT NULL, 
+    [Date_of_Birth] DATE NOT NULL, 
+    [Location] NVARCHAR(100) NOT NULL, 
+    [Email] NVARCHAR(250) NOT NULL, 
+    [C_NET_ASP] INT NULL, 
+    [CSS] INT NULL, 
+    [HTML] INT NULL, 
+    [Java] INT NULL, 
+    [JavaScript] INT NULL, 
+    [Python] INT NULL, 
+    [Python_Flask] INT NULL
+)
